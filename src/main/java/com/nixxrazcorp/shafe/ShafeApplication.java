6ba80fa99ff64ec0b52677334e31a54f245659b6
Mowowglii <erwann.razatovo@gmail.com>
2026-06-01 17:09:02 +0200

    Initial Commit

---
+++ src/main/java/com/nixxrazcorp/shafe/ShafeApplication.java
@@ -0,0 +1,13 @@
+package com.nixxrazcorp.shafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nixxrazcorp/shafe/ShafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nixxrazcorp.shafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
