39417bd5f5c7909730d5e78986c04a4f3d840f5b
Mowowglii <erwann.razatovo@gmail.com>
2026-08-14 04:02:37 +0200

    files moved

---
+++ src/main/java/com/nixxrazcorp/shafe/WebSocket/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.nixxrazcorp.shafe.WebSocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer{
+    
+    private final SignalingHandler signalingHandler;
+
+    public WebSocketConfig(SignalingHandler signalingHandler){
+        this.signalingHandler = signalingHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+        registry.addHandler(signalingHandler, "/signal")
+        .addInterceptors(new HttpSessionHandshakeInterceptor())
+        .setAllowedOrigins("*");
+    }
+    
+}
