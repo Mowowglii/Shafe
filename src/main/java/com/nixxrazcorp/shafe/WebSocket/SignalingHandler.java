2dd73dd40410ca8cdae6de8520b183cb379fa1ce
Mowowglii <erwann.razatovo@gmail.com>
2026-08-14 05:11:36 +0200

    handleTextMessage implemented and refactor of the other overrides (use of a helper method recoverRoomId)

--- src/main/java/com/nixxrazcorp/shafe/WebSocket/SignalingHandler.java
+++ src/main/java/com/nixxrazcorp/shafe/WebSocket/SignalingHandler.java
@@ -1,5 +1,8 @@
 package com.nixxrazcorp.shafe.WebSocket;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.CloseStatus;
@@ -20,10 +23,8 @@ public class SignalingHandler extends TextWebSocketHandler{
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session){
-        String query = session.getUri().getQuery();
-        if (query != null && query.contains("roomId=")){
-            String roomId = query.split("roomId=")[1].split("&")[0];
-            
+        String roomId = recoverRoomId(session);
+        if (roomId != null){
             // Note : Modify the RoomManagerService to throw errors and replace this block by a try{...}catch...
             if (!roomManagerService.joinRoom(roomId, session)){
                 System.err.println("Room Couldn't be joined");
@@ -35,16 +36,38 @@ public class SignalingHandler extends TextWebSocketHandler{
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status){
-        String query = session.getUri().getQuery();
-        if (query != null && query.contains("roomId=")){
-            String roomId = query.split("roomId=")[1].split("&")[0];
+        String roomId = recoverRoomId(session);
+        if (roomId != null){
             roomManagerService.leaveRoom(roomId, session);
+        } else {
+            System.err.println("Error in query");
         }
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message){
-        
+        String roomId = recoverRoomId(session);
+        if (roomId != null){
+            List<WebSocketSession> listOfSessions = roomManagerService.getOtherActiveSessions(roomId, session);
+
+            for (WebSocketSession ws : listOfSessions){
+                try{
+                    ws.sendMessage(message);
+                } catch (IOException e){
+                    System.err.println("Message Couldn't been sent :" + e.getMessage());
+                    roomManagerService.leaveRoom(roomId, ws);
+                }
+            }
+        }
     }
 
+    private String recoverRoomId(WebSocketSession session){
+        String roomId = null;
+        String query = session.getUri().getQuery();
+        
+        if (query != null && query.contains("roomId=")){
+            roomId = query.split("roomId=")[1].split("&")[0];
+        }
+        return roomId;
+    }
 }
