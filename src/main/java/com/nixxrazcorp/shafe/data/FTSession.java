ef3d3b0143b649a960e7226f04751de02f2e4f3e
Mowowglii <erwann.razatovo@gmail.com>
2026-08-14 03:58:22 +0200

    overriding equals to simplify room and session management

--- src/main/java/com/nixxrazcorp/shafe/data/FTSession.java
+++ src/main/java/com/nixxrazcorp/shafe/data/FTSession.java
@@ -1,6 +1,8 @@
 package com.nixxrazcorp.shafe.data;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+import org.springframework.web.socket.WebSocketSession;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,7 +12,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class FTSession {
-    private String roomId;
+    private WebSocketSession wsSession;
     private LocalDateTime createdAt;
     private boolean isActive;
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FTSession ftSession = (FTSession) o;
+        return Objects.equals(wsSession, ftSession.wsSession);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(wsSession);
+    }
 }
