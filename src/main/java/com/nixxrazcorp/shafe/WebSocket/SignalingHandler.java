9984443b3a3ad1b0aed9037c0c65bd4b11a0932e
Mowowglii <erwann.razatovo@gmail.com>
2026-08-14 04:32:23 +0200

    afterConnectionEstablished and afterConnectionClosed overrides implemented

--- src/main/java/com/nixxrazcorp/shafe/WebSocket/SignalingHandler.java
+++ src/main/java/com/nixxrazcorp/shafe/WebSocket/SignalingHandler.java
@@ -1,64 +1,50 @@
 package com.nixxrazcorp.shafe.WebSocket;
 
-import java.io.IOException;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
-
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.nixxrazcorp.shafe.Service.RoomManagerService;
+
 
 @Component
 public class SignalingHandler extends TextWebSocketHandler{
 
-    private final ConcurrentHashMap<String, CopyOnWriteArrayList<WebSocketSession>> rooms = new ConcurrentHashMap<>();
+    private final RoomManagerService roomManagerService;
+
+    public SignalingHandler(RoomManagerService roomManagerService) {
+        this.roomManagerService = roomManagerService;
+    }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session){
         String query = session.getUri().getQuery();
         if (query != null && query.contains("roomId=")){
             String roomId = query.split("roomId=")[1].split("&")[0];
-
-            session.getAttributes().put("roomId", roomId);
-
-            rooms.computeIfAbsent(roomId, k -> new CopyOnWriteArrayList<>())
-            .addIfAbsent(session);
+            
+            // Note : Modify the RoomManagerService to throw errors and replace this block by a try{...}catch...
+            if (!roomManagerService.joinRoom(roomId, session)){
+                System.err.println("Room Couldn't be joined");
+            }
         } else {
-            System.err.println("Error in Query");
+            System.err.println("Error in query");
         }
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status){
-        rooms.forEach((roomId, sessionList) -> {
-            sessionList.remove(session);
-            if (sessionList.isEmpty()){
-                rooms.remove(roomId);
-            }
-        });
+        String query = session.getUri().getQuery();
+        if (query != null && query.contains("roomId=")){
+            String roomId = query.split("roomId=")[1].split("&")[0];
+            roomManagerService.leaveRoom(roomId, session);
+        }
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message){
         
-        // Recover the room of the session
-        String roomId = (String) session.getAttributes().get("roomId");
-
-        // Recover the sessions in the room
-        CopyOnWriteArrayList<WebSocketSession> sessions = rooms.get(roomId);
-
-        for (WebSocketSession webSocketSession : sessions){
-            if (webSocketSession.isOpen() && !webSocketSession.getId().equals(session.getId())){
-                try {
-                    webSocketSession.sendMessage(message);
-                } catch (IOException e){
-                    System.err.println("Couldn't send message : " + e.getMessage());
-                }
-            }
-        }
     }
 
 }
