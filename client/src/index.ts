1c566f833b22290f7edd6bd232d84baf2bc4d0f7
Mowowglii <erwann.razatovo@gmail.com>
2026-09-08 14:45:24 +0200

    index file added

---
+++ client/src/index.ts
@@ -0,0 +1,4 @@
+import { WebSocketService } from "./signaling/ws.service.js";
+import { PeerManagerService } from "./signaling/pm.service.js";
+
+const REST_API : string = import.meta.env.REST_ENDPOINT as string;
