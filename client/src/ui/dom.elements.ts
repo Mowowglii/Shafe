3b53fb94377491c0937ec56bca875d86cff88106
Mowowglii <erwann.razatovo@gmail.com>
2026-09-08 14:45:48 +0200

    document element recoverer file

---
+++ client/src/ui/dom.elements.ts
@@ -0,0 +1,5 @@
+export const createRoomBtn = document.getElementById("create-room-btn") as HTMLButtonElement;
+export const roomIdInput = document.getElementById("room-id-input") as HTMLInputElement;
+export const joinRoomBtn = document.getElementById("join-room-btn") as HTMLButtonElement;
+export const fileInput = document.getElementById("file-input") as HTMLInputElement;
+export const sendFileBtn = document.getElementById("send-file-btn") as HTMLButtonElement;
